53331d5764352edaf61998f860217a475bc13921
Nathan Shubert-Harbison <nathan@carterhales.com>
2016-01-18 09:26:18 -0800

    Different approach to menu classes (nuke and pave) based on a suggestion from @nmwd

--- includes/menu_classes.php
+++ includes/menu_classes.php
@@ -9,15 +9,77 @@
 	 * @internal called by `nav_menu_css_class` filter
 	 *
 	 */
-	function pp_nav_class($classes, $item, $args) {
+	add_filter('nav_menu_css_class', function($classes, $item, $args) {
 
-		$classes[] = strtolower(str_replace("--", "", preg_replace("([^a-zA-Z])", "-", $item->title)));
+		// Lowercase title
+		$title = strtolower(str_replace("--", "", preg_replace("([^a-zA-Z])", "-", $item->title)));
+
+		// Crazy town, start from scratch
+		$old_classes = $classes;
+		$classes = [];
+
+		// Loop through the classes, add active and children
+		foreach ( $old_classes as $class ) {
+
+			// Active
+			if ( strstr($class, "current") && !in_array('is-active', $classes)) {
+				$classes[] = 'is-active';
+			}
+
+			// Open the active item by default
+			if ( in_array('is-active', $classes) && $item->menu_item_parent == 0 && !in_array('is-open', $classes) ) {
+				$classes[] = 'is-open';
+			}
+
+			// Preserve any BEM classes
+			if ( strstr($class, "--") || strstr($class, "__") ) {
+				$classes[] = $class;
+			}
+
+		}
+
+		// Children
+		if ( in_array("menu-item-has-children", $old_classes) ) {
+			$classes[] = 'has-children';
+		}
 
 		// Add BEM class
 		$classes[] = $args->container_class . '__item';
 
-	    return $classes;
+		// Add BEM modifier for items
+		$classes[] = $args->container_class . '__item--' . $title;
 
-	}
+		return $classes;
 
-	add_filter('nav_menu_css_class', 'pp_nav_class', 10, 3);
+	}, 10, 3);
+
+
+	/**
+	 * Hook into menu objects before HTML
+	 */
+	add_filter('wp_nav_menu_objects', function($items){
+
+		foreach ( $items as $item ) {
+
+			// Add a primary-nav__child-item class
+			if ( $item->menu_item_parent > 0 ) {
+				$item->classes[] = 'primary-nav__child-item';
+			}
+
+		}
+
+		return $items;
+
+	});
+
+
+	/**
+	 * Muck with the HTML
+	 */
+	add_filter('wp_nav_menu_items', function($items) {
+
+		$items = str_replace("sub-menu", "primary-nav__children", $items);
+
+		return $items;
+
+	});
