33fe563b31c99cb4ae32dbef4526064c6335d34e
Nathan Shubert-Harbison <nathan@carterhales.com>
2016-06-14 16:27:53 -0700

    Default search field placeholder value (thanks for the reminder @alanchao)

--- searchform.php
+++ searchform.php
@@ -1,5 +1,15 @@
+<?php
+
+	/**
+	 * Create the placeholder value
+	 *
+	 */
+	$placeholder = isset( $_GET['s'] ) && $_GET['s'] ? $_GET['s'] : 'Search';
+
+?>
+
 <form action="<?php echo home_url(); ?>" id="searchform" method="get" class="search">
 	<label for="s" class="u-screen-reader"><?php _e('Search for:', get_bloginfo('name') ); ?></label>
-	 <input class="search__input" type="text" id="s" name="s" value="" <?php if (isset($_GET["s"])) echo 'placeholder="' . $_GET["s"] . '"'; ?> />
+	 <input class="search__input" type="text" id="s" name="s" value="" placeholder="<?php echo $placeholder; ?>" />
 	 <input class="search__submit" type="submit" value="Search" id="searchsubmit" />
 </form>
