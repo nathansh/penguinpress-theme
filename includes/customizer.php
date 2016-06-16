74176d5eed717b21f7fb1dbe36eb336866650a55
Nathan Shubert-Harbison <nathan@carterhales.com>
2016-06-16 15:38:53 -0700

    remove site icon and tagline from customizer

--- includes/customizer.php
+++ includes/customizer.php
@@ -7,5 +7,7 @@
 add_action( 'customize_register', function( $wp_customize ) {
 
 	$wp_customize->remove_section( 'static_front_page' );
+	$wp_customize->remove_control( 'blogdescription' );
+	$wp_customize->remove_control( 'site_icon' );
 
 } );
